14d367c49d62129778bb6bfe571b6c36d135f549
Michael Sevilla <mikesevilla3@gmail.com>
2017-04-13 10:48:28 -0700

    image: add plfs client container
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- images/entrypoint.sh
+++ images/entrypoint.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
-/bin/bash
+
+MOUNT="/mnt/plfs"
+BACKE="posix:///mnt/pstore1"
+
+if [ ! -z $1 ]; then
+  MOUNT="$1"
+fi
+if [ ! -z $2 ]; then
+  BACKE="$2"
+fi
+
+echo "- global_params:"        >  /etc/plfsrc
+echo "  threadpool_size: 64"   >> /etc/plfsrc
+echo "- mount_point: $MOUNT"   >> /etc/plfsrc
+echo "  workload: shared_file" >> /etc/plfsrc
+echo "  backends:"             >> /etc/plfsrc
+echo "    - location: $BACKE"  >> /etc/plfsrc
+
+echo "-- PLFS configuration"
+cat /etc/plfsrc
+
+echo "-- Setup directories"
+plfs_check_config -mkdir
+
+echo "-- Mount it"
+plfs -o -f -d $MOUNT
