b9f7a60581158cb349f58ca6731051e31025726b
Michael Sevilla <mikesevilla3@gmail.com>
2017-12-07 13:53:11 -0800

    benchmark: plfs scaling
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/n1/run.sh
+++ experiments/n1/run.sh
@@ -15,7 +15,7 @@ mkdir -p results/logs || true
 # configure ceph and setup results directory
 cp site/* site/roles/ceph-ansible || true
 cp -r site/group_vars site/roles/ceph-ansible/
-cp site/inventory/all site/hosts
+cp site/inventory/6 site/hosts
 cp site/hosts site/roles/ceph-ansible/hosts
 
 # setup
@@ -24,7 +24,7 @@ $CEPH_ANSIBLE ceph.yml cephfs.yml
 $SRL_ANSIBLE ceph_pgs.yml ceph_monitor.yml ceph_wait.yml
  
 # benchmark
-for i in `ls ../conf/hosts/`; do
+for i in `ls site/inventory/`; do
   cp site/inventory/${i} site/hosts
   cp site/hosts site/roles/ceph-ansible/hosts
   ./ansible-playbook.sh -e nclients=${i} ../workloads/plfs.yml
