b988a53804fccd1bf4e6713b7430cd8eeb873136
Michael Sevilla <mikesevilla3@gmail.com>
2018-01-22 19:21:06 -0800

    paper: shore up examples and update biblio
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- paper/build.sh
+++ paper/build.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
 rm -r _minted-paper paper.aux paper.bbl paper.blg paper.log paper.out paper.synctex.gz build.log >> /dev/null 2>&1
-set -ex
+docker stop latex
+docker rm latex
+set -x
+cp biblio/references.bib paper.bib
 
 # Build the paper
-docker rm -f latex || true
-  #ivotron/texlive_v2:latest -c \
-docker run \
+docker run --rm \
   --name latex \
   --entrypoint=/bin/bash \
   -v `pwd`/:/mnt \
   michaelsevilla/texlive:latest -c \
     "cd /mnt ; \
-     pdflatex -synctex=1 -interaction=nonstopmode -shell-escape paper && \
-     bibtex paper && \
-     pdflatex -synctex=1 -interaction=nonstopmode -shell-escape paper && \
-     pdflatex -synctex=1 -interaction=nonstopmode -shell-escape paper" &> build.log
+     pdflatex -synctex=1 -interaction=nonstopmode -shell-escape paper; \
+     bibtex paper; \
+     pdflatex -synctex=1 -interaction=nonstopmode -shell-escape paper; \
+     pdflatex -synctex=1 -interaction=nonstopmode -shell-escape paper;" &> build.log
 
-ERR=$?
-if [ $ERR != "0" ] ; then
-  echo "ERROR: $ERR"
-  cat build.log
-  exit 1
-fi
+#ERR=$?
+#if [ $ERR != "0" ] ; then
+#  set +x
+#  echo "ERROR: $ERR"
+#  cat build.log
+#  exit 1
+#fi
 
-set +e
+set +x
 rm -r _minted-paper paper.aux paper.bbl paper.blg paper.log paper.out paper.synctex.gz build.log >> /dev/null 2>&1
 
 echo "SUCCESS"
-exit 0
+
