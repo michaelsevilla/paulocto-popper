a7c7993729e035bf43788d817d506beb392b5deb
Michael Sevilla <mikesevilla3@gmail.com>
2018-01-25 16:23:54 -0800

    paper: skeleton, benchmark: workflow
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/hep/run.sh
+++ experiments/hep/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-docker run --rm -it \
-  -v ~/Desktop/:/root \
-  -w /root \
- --entrypoint=/bin/bash \
- rootproject/root-ubuntu16 
-#-c "root ./2AC36403-8E7E-E711-A599-02163E01366D.root"
+echo "Setup 1: read from the ROOT file"
+.x read_baskets_from_ROOT.cpp ("./2AC36403-8E7E-E711-A599-02163E01366D.root")
+
+echo "Setup 3: read from namespace"
+.x write_baskets_to_file.cpp("./2AC36403-8E7E-E711-A599-02163E01366D.root")
+.x read_baskets_from_file.cpp("branchListFile.txt")
+
+
+# TODO: figure out the timing
+# TODO: get read_baskets_from_ROOT to work
+# TODO: get these scripts to setup everything
+# TODO: deploy on cloudlab and run
