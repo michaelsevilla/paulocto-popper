4e42728e261116cb5e0ba57cea9654dcc9c0c94e
Michael Sevilla <mikesevilla3@gmail.com>
2018-01-29 10:25:02 -0800

    benchmark: transfer baseline experiment from cudele-popper
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/hep/run.sh
+++ experiments/hep/run.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+if [ -z $SRC ]; then
+  echo "Please tell me where the source code is by populating \$SRC"
+  exit
+fi
+
+echo "Setup 3: read from namespace"
+docker run --rm -it \
+  -w /root \
+  -v `pwd`:/scripts \
+  -v $SRC:/root \
+  --entrypoint=root \
+  rootproject/root-ubuntu16
+  -b -q .x root/src/read_baskets_from_file.cpp\(\"branchListFileTest.txt\"\)
+
+
+exit 
 echo "Setup 1: read from the ROOT file"
 .x read_baskets_from_ROOT.cpp ("./2AC36403-8E7E-E711-A599-02163E01366D.root")
 
-echo "Setup 3: read from namespace"
 .x write_baskets_to_file.cpp("./2AC36403-8E7E-E711-A599-02163E01366D.root")
 .x read_baskets_from_file.cpp("branchListFile.txt")
 
