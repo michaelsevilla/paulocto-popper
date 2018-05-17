9bcae26cf796d7cf09b01c7e121f0787dde72f8e
Michael Sevilla <mikesevilla3@gmail.com>
2018-05-17 12:25:36 -0700

    acks: fixes #28
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- paper/build.sh
+++ paper/build.sh
@@ -11,7 +11,7 @@ docker run --rm \
   --name latex \
   --entrypoint=/bin/bash \
   -v `pwd`/:/mnt \
-  michaelsevilla/texlive:latest -c \
+  michaelsevilla/texlive:acmart-popper -c \
     "cd /mnt ; \
      pdflatex -synctex=1 -interaction=nonstopmode -shell-escape paper; \
      bibtex paper; \
